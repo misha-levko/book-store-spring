72de5dc91d55998975290e6e23b570ea93393bab
misha-levko <mykhailo.levko@gmail.com>
2024-06-22 17:05:47 -0400

    first commit

---
+++ src/test/java/mate/academy/bookstorespring/BookStoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.bookstorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
