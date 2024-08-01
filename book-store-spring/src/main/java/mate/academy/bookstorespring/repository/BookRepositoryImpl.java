b6f65a1a1c9522c7f34c1e2f084c79ceda9d2936
Mykhailo Levko <mykhailo.levko@gmail.com>
2024-07-31 21:13:14 -0400

    Fixing BookRepositoryImpl class

--- book-store-spring/src/main/java/mate/academy/bookstorespring/repository/BookRepositoryImpl.java
+++ book-store-spring/src/main/java/mate/academy/bookstorespring/repository/BookRepositoryImpl.java
@@ -1,51 +1,59 @@
 package mate.academy.bookstorespring.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
 import java.util.List;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
 import mate.academy.bookstorespring.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
-@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final EntityManagerFactory entityManagerFactory;
+    private final SessionFactory sessionFactory;
+
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public Book createBook(Book book) {
-        EntityTransaction transaction = null;
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            transaction = entityManager.getTransaction();
-            transaction.begin();
-            entityManager.persist(book);
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
             transaction.commit();
-            return book;
-        } catch (RuntimeException e) {
-            if (transaction != null && transaction.isActive()) {
+        } catch (Exception e) {
+            if (transaction != null) {
                 transaction.rollback();
             }
-            throw e;
+            throw new RuntimeException("Can't create a book: " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
+        return book;
     }
 
     @Override
     public List<Book> getAll() {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            return entityManager
-                    .createQuery("SELECT b FROM Book b", Book.class)
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT b FROM Book b", Book.class)
                     .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books from DB");
         }
     }
 
     @Override
     public Optional<Book> getBookById(Long id) {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            Book book = entityManager.find(Book.class, id);
-            return Optional.ofNullable(book);
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.find(Book.class, id));
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get book by id: " + id);
         }
     }
 }
-
