f7e1f6f5257e262cb8b0dc582f7861803f9f98f6
Mykhailo Levko <mykhailo.levko@gmail.com>
2024-10-14 15:11:17 -0400

    Added: CustomGlobalExceptionHandler class; validation for DTO classes; dependency org.hibernate.validator;

--- book-store-spring/src/main/java/mate/academy/bookstorespring/controller/BookController.java
+++ book-store-spring/src/main/java/mate/academy/bookstorespring/controller/BookController.java
@@ -1,5 +1,6 @@
 package mate.academy.bookstorespring.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstorespring.dto.BookDto;
@@ -33,7 +34,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.createBook(bookDto);
     }
 
--- book-store-spring/src/main/java/mate/academy/bookstorespring/dto/CreateBookRequestDto.java
+++ book-store-spring/src/main/java/mate/academy/bookstorespring/dto/CreateBookRequestDto.java
@@ -1,13 +1,21 @@
 package mate.academy.bookstorespring.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
